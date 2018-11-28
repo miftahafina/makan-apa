cdd7c6eb9220dd482224563a1bf7df2dc0d1674f
Miftah Afina <miftah.afina@gmail.com>
2018-11-28 11:35:13 +0700

    Add border on thumbnail

--- components/SideDishBox.js
+++ components/SideDishBox.js
@@ -16,17 +16,18 @@ export default class SideDishBox extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    width: 120,
+    width          : 120,
     backgroundColor: '#fff',
-    borderColor: 'yellow'
   },
-
+  
   box: {
     width          : 100,
     height         : 100,
     margin         : 10,
     borderRadius   : 20,
-    backgroundColor: 'gray'
+    backgroundColor: 'gray',
+    borderWidth    : 1,
+    borderColor    : '#ededed'
   },
 
   text: {
