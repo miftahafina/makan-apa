0082e08464db9a7ab190cf3564db986f7f8b5bfb
Miftah Afina <miftah.afina@gmail.com>
2018-11-29 21:40:58 +0700

    change Date to moment

--- App.js
+++ App.js
@@ -16,39 +16,35 @@ export default class App extends React.Component {
     dayId  : ['Ahad', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jum\'at', 'Sabtu'],
     monthId: ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'],
 
-    day  : new Date().getDay(),
-    date : new Date().getDate(),
-    month: new Date().getMonth(),
-    year : new Date().getFullYear(),
-    week : Moment().week(),
+    moment: Moment(),
     
     schedule: schedule,
-    menu_a: [48,52,4, 8,12,16,20,24,28,32,36,40,44],
-    menu_b: [49, 1,5, 9,13,17,21,25,29,33,37,41,45],
-    menu_c: [50, 2,6,10,14,18,22,26,30,34,38,42,46],
-    menu_d: [51, 3,7,11,15,19,23,27,31,35,39,43,47]
+    menu_a  : [48,52,4, 8,12,16,20,24,28,32,36,40,44],
+    menu_b  : [49, 1,5, 9,13,17,21,25,29,33,37,41,45],
+    menu_c  : [50, 2,6,10,14,18,22,26,30,34,38,42,46],
+    menu_d  : [51, 3,7,11,15,19,23,27,31,35,39,43,47]
   }
 
   render() {
-    const { day, date, month, year, week, schedule, dayId, monthId, menu_a, menu_b, menu_c, menu_d, } = this.state;
+    const { moment, day, date, month, year, week, schedule, dayId, monthId, menu_a, menu_b, menu_c, menu_d, } = this.state;
     const { container, verticalScroll } = styles;
 
     let type = 0;
     let typeId = '';
 
-    if (menu_a.indexOf(week) >= 0) {
+    if (menu_a.indexOf(moment.week()) >= 0) {
       type   = 0;
       typeId = 'A';
 
-    } else if (menu_b.indexOf(week) >= 0) {
+    } else if (menu_b.indexOf(moment.week()) >= 0) {
       type   = 1;
       typeId = 'B';
 
-    } else if (menu_c.indexOf(week) >= 0) {
+    } else if (menu_c.indexOf(moment.week()) >= 0) {
       type   = 2;
       typeId = 'C';
 
-    } else if (menu_d.indexOf(week) >= 0) {
+    } else if (menu_d.indexOf(moment.week()) >= 0) {
       type   = 3;
       typeId = 'D';
     }
@@ -57,13 +53,13 @@ export default class App extends React.Component {
       <View style={container}>
         <ImageBackground source={require('./assets/img/bg.png')} style={{width: '100%', height: '100%'}}>
           <ScrollView style={verticalScroll} showsVerticalScrollIndicator={false}>
-            <Today day={dayId[day]}
-            date={date + ' ' + monthId[month] + ' ' + year}
-            week={week} typeId={typeId}/>
+            <Today day={dayId[moment.day()]}
+            date={moment.date() + ' ' + monthId[moment.month()] + ' ' + moment.year()}
+            week={moment.week()} typeId={typeId}/>
             
-            <SideDishScroller meal='breakfast' mealId='Pagi' day={day} type={type}/>
-            <SideDishScroller meal='lunch' mealId='Siang' day={day} type={type}/>
-            <SideDishScroller meal='dinner' mealId='Malam' day={day} type={type}/>
+            <SideDishScroller meal='breakfast' mealId='Pagi' day={moment.day()} type={type}/>
+            <SideDishScroller meal='lunch' mealId='Siang' day={moment.day()} type={type}/>
+            <SideDishScroller meal='dinner' mealId='Malam' day={moment.day()} type={type}/>
 
             <CreditFooter />
           </ScrollView>
